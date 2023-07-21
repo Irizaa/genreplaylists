603fd45b204a35cdbe8de26a95e1084f70d565b7
daniel <squaredfreezer@gmail.com>
2023-07-20 21:34:56 -0400

    /results updates: add enough functionality to create new playlists based on genre of each song.

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+const { promisify } = require('util')
+const delay = promisify(setTimeout)
 require ('dotenv').config()
 const axios = require('axios')
 import express from 'express';
@@ -30,38 +32,89 @@ app.get("/login", (req, res) => {
 axios.defaults.baseURL = 'https://api.spotify.com/v1/'
 
 const getLikedSongs = async (access_token:any) => {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
-    let currLink = 'me/tracks?offset=0&limit=50';
-    let songs: any[] = [];
+    
+    let currLink = 'me/tracks?offset=0&limit=50'
+    let songs: any[] = []
   
-    while (currLink !== null) {
+    // while (currLink !== null) { // Commenting this out for easier testing. Will add it back in when functionality is guaranteed.
       try {
-        const response = await axios.get(currLink);
-        songs.push(...response.data.items);
-        currLink = response.data.next;
+        const response = await axios.get(currLink)
+        songs.push(...response.data.items)
+        currLink = response.data.next
       } catch (err) {
-        console.error(err);
-        throw new Error('Failed to retrieve liked songs.');
+        console.error(err)
+        throw new Error('Failed to retrieve liked songs.')
       }
-    }
+    // }
   
-    return songs;
+    return songs
   };
   
+const createPlaylist = (genre: string, userID: string) => {
+    axios({
+        method: 'post',
+        url: `users/${userID}/playlists`,
+        data: {
+            "name": `${genre}`
+        }
+    })
+}  
+const getUserID = () => {
+    return axios.get('me')
+    .then((response:any) => response.data.id)
+    .catch((error:any) => {
+        console.log(error);
+        throw error; // Rethrow the error to be caught later, if needed
+    });
+}
 
-app.get('/results', (req, res) => {
+app.get('/results', async (req, res) => {
     const access_token = req.query.access_token
+    axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`
+    let userID:any
+    try {
+        userID = await getUserID()
+        console.log(userID)
+    } catch (error) {
+        console.log(error)
+    }
+
+    console.log(userID)
+    let map:string[] = []
+
     getLikedSongs(access_token)
     .then((songs) => {
-      console.log(songs); // Do something with the liked songs
+      for(let i = 0; i < songs.length; i++) {
+        let currArtist = songs[i].track.artists[0].id
+        console.log(currArtist)
+        
+        axios.get(`/artists/${currArtist}`)
+        .then((response:any) => {
+            let currGenres = response.data.genres
+            for(let j = 0; j < currGenres.length; j++) {
+                let currGenre = currGenres[j]
+                console.log(currGenre)
+                
+                if(!map.includes(currGenre)) {
+                    createPlaylist(currGenre, userID)
+                    map.push(currGenre)
+                }
+            }
+        })
+        .catch((error:any) => {
+            console.log(error)
+        })
+        
+      }
     })
     .catch((err) => {
       console.error(err);
     });
-    // Iterate through every song in user's liked library.
-    // At every song, check through all the genres of that song.
-    // Check if playlist with that genre name exists. If it doesn't, create it and add the song.
-    // If it does exist, check if that song exists within the playlist. If it doesn't, add the song. If it does, do nothing.
+    // Iterate through every song in user's liked library. \DONE
+    // At every song, get the Spotify ID of the artist for that song. \DONE
+    // Then, with that ID, get the artist's genres. (https://developer.spotify.com/documentation/web-api/reference/get-an-artist) \DONE
+    // Map ====> Key(STRING): Genre | Value(STRING[]): List of songs.
+    // If the genre does not exist as a playlist, create it. Then, add the song to the genre playlist.
     res.send('Hey!')
 })
 
